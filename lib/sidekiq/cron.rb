2884a709c323809ed4c341cd8142e503ad9e2f38
Ondrej Bartas <ondrej@bartas.cz>
2013-08-25 15:07:11 +0200

    [ADD] Main Cron module - register web extension when web is required

---
+++ lib/sidekiq/cron.rb
@@ -0,0 +1,25 @@
+begin
+  require "sidekiq/web"
+rescue LoadError
+  # client-only usage
+end
+
+require "sidekiq/cron/job"
+require "sidekiq/cron/poller"
+require "sidekiq/cron/web_extension"
+
+module Sidekiq
+  module Cron
+  end
+end
+
+if defined?(Sidekiq::Web)
+  Sidekiq::Web.register Sidekiq::Cron::WebExtension
+
+  if Sidekiq::Web.tabs.is_a?(Array)
+    # For sidekiq < 2.5
+    Sidekiq::Web.tabs << "cron"
+  else
+    Sidekiq::Web.tabs["Cron"] = "cron"
+  end
+end
