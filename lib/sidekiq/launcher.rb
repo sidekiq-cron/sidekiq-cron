a07015301643c3a2c31e1d14290382e178b79ab4
Ondrej Bartas <ondrej@bartas.cz>
2013-08-25 15:08:38 +0200

    [ADD] inject Cron::Poller to Sidekiq::auncher

---
+++ lib/sidekiq/launcher.rb
@@ -0,0 +1,46 @@
+require 'sidekiq/cron/poller'
+
+
+# For Cron we need to add some methods to Launcher
+# so look at the code bellow.
+#
+# we are creating new cron poller instance and
+# adding start and stop commands to launcher
+module Sidekiq
+  class Launcher
+
+    #Add cron poller to launcher
+    attr_reader :cron_poller
+
+
+    #remember old initialize
+    alias_method :old_initialize, :initialize
+
+    #add cron poller and execute normal initialize of Sidekiq launcher
+    def initialize(options)
+      @cron_poller  = Sidekiq::Cron::Poller.new
+      old_initialize options
+    end
+
+
+    #remember old run
+    alias_method :old_run, :run
+
+    #execute normal run of launcher and run cron poller
+    def run
+      old_run
+      cron_poller.async.poll(true)
+    end
+
+
+    #remember old stop
+    alias_method :old_stop, :stop
+
+    #execute normal stop of launcher and stop cron poller
+    def stop
+      cron_poller.async.terminate if poller.alive?
+      old_stop
+    end
+
+  end
+end
