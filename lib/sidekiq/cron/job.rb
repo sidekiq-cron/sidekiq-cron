a273fccc1f1ded0a64bc2a5ec9bf4632d2b0f860
David Warburton <david.warburton@gmail.com>
2020-06-18 12:07:09 -0400

    validate jobs in Job.find
    In the case of two or more Sidekiq workers the job hash in Redis can be
    left in a partly deleted state after one worker deletes the job and
    another worker sets the last_enqueue_time value. If this happens
    before this commit then calls to Job.find will return an invalid job
    that doesn't have a  name (or any properties but last_enqueue_time)
    and can't be destroyed. After this commit nil is returned if the Job is
    not valid.

--- lib/sidekiq/cron/job.rb
+++ lib/sidekiq/cron/job.rb
@@ -233,7 +233,7 @@ module Sidekiq
             output = Job.new conn.hgetall( redis_key(name) )
           end
         end
-        output
+        output if output.valid?
       end
 
       # create new instance of cron job
