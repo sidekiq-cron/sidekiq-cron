50a2e1b3eae16bae2ce57e98024d042a53ec6905
Hans Lemuet <hans@spone.net>
2022-02-11 12:48:12 +0100

    Allow natural language parsing with Fugit

--- lib/sidekiq/cron/job.rb
+++ lib/sidekiq/cron/job.rb
@@ -417,7 +417,7 @@ module Sidekiq
           errors << "'cron' must be set"
         else
           begin
-            @parsed_cron = Fugit.do_parse_cron(@cron)
+            @parsed_cron = Fugit.do_parse(@cron)
           rescue => e
             errors << "'cron' -> #{@cron.inspect} -> #{e.class}: #{e.message}"
           end
@@ -557,7 +557,7 @@ module Sidekiq
       private
 
       def parsed_cron
-        @parsed_cron ||= Fugit.parse_cron(@cron)
+        @parsed_cron ||= Fugit.parse(@cron)
       end
 
       def not_enqueued_after?(time)
