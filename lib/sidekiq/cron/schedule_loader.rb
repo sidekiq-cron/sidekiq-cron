552844593fbaf7c683f340ebf26d3a63ca9b1f0c
Sandip Mane <sandip2490@gmail.com>
2022-06-11 00:15:46 +0530

    #250 - Adds support for auto-loading the config/schedule.yml file

---
+++ lib/sidekiq/cron/schedule_loader.rb
@@ -0,0 +1,16 @@
+require "sidekiq"
+require "sidekiq/cron/job"
+
+if Sidekiq.server?
+  Sidekiq.configure_server do |config|
+    schedule_file = config.options[:cron_schedule_file] || "config/schedule.yml"
+
+    if File.exist?(schedule_file)
+      config.on(:startup) do
+        if File.exist?(schedule_file)
+          Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+        end
+      end
+    end
+  end
+end
