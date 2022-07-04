55781b36bd5eaeff07499efa57e1d0906b77af42
Cuong Lam <thanhcuong1990@gmail.com>
2022-07-04 11:58:30 +0700

    feedback updates.

--- lib/sidekiq/options.rb
+++ lib/sidekiq/options.rb
@@ -3,7 +3,16 @@ require 'sidekiq'
 module Sidekiq
   module Options
     def self.[](key)
-      Sidekiq.respond_to?(:[]) ? Sidekiq[key] : Sidekiq.options[key]
+      new_version? ? Sidekiq[key] : Sidekiq.options[key]
+    end
+
+    def self.set_config(config, key, value)
+      new_version? ? config[key] = value : config.options[key] = value
+    end
+
+    # sidekiq --version >= 6.5.0
+    def self.new_version?
+      @new_version ||= Sidekiq.respond_to?(:[])
     end
   end
 end
