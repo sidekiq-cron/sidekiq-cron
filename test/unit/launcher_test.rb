91af2518cd02a028f758298e9c2690fdfde6aa64
Philip Dubé <phdub@microsoft.com>
2022-11-01 14:57:58 +0000

    Fix whitespace

--- test/unit/launcher_test.rb
+++ test/unit/launcher_test.rb
@@ -11,7 +11,7 @@ describe 'Cron launcher' do
         assert_equal Sidekiq::Cron::Launcher::DEFAULT_POLL_INTERVAL, options[:cron_poll_interval]
       end
 
-	  Sidekiq::Launcher.new(Sidekiq.const_defined?(:default_configuration) ? Sidekiq.default_configuration : Sidekiq)
+     Sidekiq::Launcher.new(Sidekiq.const_defined?(:default_configuration) ? Sidekiq.default_configuration : Sidekiq)
     end
 
     it 'initializes poller with the configured poll interval' do
@@ -20,14 +20,14 @@ describe 'Cron launcher' do
       end
 
       Sidekiq::Options[:cron_poll_interval] = 99
-	  Sidekiq::Launcher.new(Sidekiq.const_defined?(:default_configuration) ? Sidekiq.default_configuration : Sidekiq)
+      Sidekiq::Launcher.new(Sidekiq.const_defined?(:default_configuration) ? Sidekiq.default_configuration : Sidekiq)
     end
 
     it 'does not initialize the poller when interval is 0' do
       Sidekiq::Cron::Poller.expects(:new).never
 
       Sidekiq::Options[:cron_poll_interval] = 0
-	  Sidekiq::Launcher.new(Sidekiq.const_defined?(:default_configuration) ? Sidekiq.default_configuration : Sidekiq)
+      Sidekiq::Launcher.new(Sidekiq.const_defined?(:default_configuration) ? Sidekiq.default_configuration : Sidekiq)
     end
   end
 end
