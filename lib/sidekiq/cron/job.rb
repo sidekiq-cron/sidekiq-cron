ac533330071897d1f8b80cadab53ffdd2ec7ac1c
Robert Steuck <robert.steuck@gmail.com>
2022-11-29 08:22:13 +0100

    always enque via active_job interface when defined in cron job config

--- lib/sidekiq/cron/job.rb
+++ lib/sidekiq/cron/job.rb
@@ -57,7 +57,7 @@ module Sidekiq
 
         jid =
           if klass_const
-            if defined?(ActiveJob::Base) && klass_const < ActiveJob::Base
+            if is_active_job?(klass_const)
               enqueue_active_job(klass_const).try :provider_job_id
             else
               enqueue_sidekiq_worker(klass_const)
@@ -75,8 +75,8 @@ module Sidekiq
         Sidekiq.logger.debug { "enqueued #{@name}: #{@message}" }
       end
 
-      def is_active_job?
-        @active_job || defined?(ActiveJob::Base) && Sidekiq::Cron::Support.constantize(@klass.to_s) < ActiveJob::Base
+      def is_active_job?(klass = Sidekiq::Cron::Support.constantize(@klass.to_s))
+        @active_job || defined?(ActiveJob::Base) && klass < ActiveJob::Base
       rescue NameError
         false
       end
