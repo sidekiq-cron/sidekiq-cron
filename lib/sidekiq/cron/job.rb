555336cd3f26d7fc50fe07ee5f9bbb20d63fc2ec
Ryota Arai <ryota.arai@gmail.com>
2023-03-03 00:08:18 +0900

    Make last_enqueue_time be always an instance of Time. (#354)

--- lib/sidekiq/cron/job.rb
+++ lib/sidekiq/cron/job.rb
@@ -45,7 +45,7 @@ module Sidekiq
 
       # Enqueue cron job to queue.
       def enque! time = Time.now.utc
-        @last_enqueue_time = time.strftime(LAST_ENQUEUE_TIME_FORMAT)
+        @last_enqueue_time = time
 
         klass_const =
             begin
@@ -409,7 +409,7 @@ module Sidekiq
           active_job: @active_job ? "1" : "0",
           queue_name_prefix: @active_job_queue_name_prefix,
           queue_name_delimiter: @active_job_queue_name_delimiter,
-          last_enqueue_time: @last_enqueue_time.to_s,
+          last_enqueue_time: serialized_last_enqueue_time,
           symbolize_args: symbolize_args? ? "1" : "0",
         }
 
@@ -477,7 +477,7 @@ module Sidekiq
       def save_last_enqueue_time
         Sidekiq.redis do |conn|
           # Update last enqueue time.
-          conn.hset redis_key, 'last_enqueue_time', @last_enqueue_time
+          conn.hset redis_key, 'last_enqueue_time', serialized_last_enqueue_time
         end
       end
 
@@ -657,6 +657,10 @@ module Sidekiq
       # Give Hash returns array for using it for redis.hmset
       def hash_to_redis hash
         hash.flat_map{ |key, value| [key, value || ""] }
+      end
+
+      def serialized_last_enqueue_time
+        @last_enqueue_time&.strftime(LAST_ENQUEUE_TIME_FORMAT)
       end
     end
   end
