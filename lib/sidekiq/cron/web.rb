3c347c337d3fc1c11924a830db201a39b31142ee
Leo Arnold <leoarnold@users.noreply.github.com>
2025-03-08 21:35:04 +0100

    Add support for Sidekiq v8 (#531)

--- lib/sidekiq/cron/web.rb
+++ lib/sidekiq/cron/web.rb
@@ -3,7 +3,16 @@ require "sidekiq/cron/job"
 require "sidekiq/cron/namespace"
 
 if defined?(Sidekiq::Web)
-  if Gem::Version.new(Sidekiq::VERSION) >= Gem::Version.new('7.3.0')
+  if Gem::Version.new(Sidekiq::VERSION) >= Gem::Version.new('8.0.0')
+    Sidekiq::Web.configure do |config|
+      config.register(
+        Sidekiq::Cron::WebExtension, # Class which contains the HTTP actions, required
+        name: "cron", # the name of the extension, used to namespace assets
+        tab: "Cron", # labels(s) of the UI tabs
+        index: "cron", # index route(s) for each tab
+      )
+    end
+  elsif Gem::Version.new(Sidekiq::VERSION) >= Gem::Version.new('7.3.0')
     Sidekiq::Web.register(
       Sidekiq::Cron::WebExtension, # Class which contains the HTTP actions, required
       name: "cron", # the name of the extension, used to namespace assets
--- sidekiq-cron.gemspec
+++ sidekiq-cron.gemspec
@@ -33,8 +33,8 @@ Gem::Specification.new do |s|
 
   s.add_development_dependency("minitest", "~> 5.15")
   s.add_development_dependency("mocha", "~> 2.1")
-  s.add_development_dependency("rack", "~> 2.2")
-  s.add_development_dependency("rack-test", "~> 1.1")
+  s.add_development_dependency("rack", ">= 2.2")
+  s.add_development_dependency("rack-test", ">= 1.1")
   s.add_development_dependency("rake", "~> 13.0")
   s.add_development_dependency("simplecov", "~> 0.21")
   s.add_development_dependency("simplecov-cobertura", "~> 2.1")
